cbc2d4ce9a154c0f3e6ab963a72bef8a96d7260f
Caleb White <calebadwhite@gmail.com>
2023-11-28 16:21:07 -0500

    tried updating the welcome message but it didn't take, will have to  investigate further

--- gs-spring-boot-main/complete/src/main/java/com/example/springboot/HelloController.java
+++ gs-spring-boot-main/complete/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "Greetings from Spring Boot! This application has now been dockerized This application has now been dockerized";
 	}
 
 }
