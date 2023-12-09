8e8b5c28f5e8672315eda1d084ea54d9bd952389
Caleb White <calebadwhite@gmail.com>
2023-12-09 18:48:40 -0500

    updated controllers, dockerfile

--- gs-spring-boot-main/complete/src/main/java/com/example/springboot/HelloController.java
+++ gs-spring-boot-main/complete/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot! This application has now been dockerized This application has now been dockerized";
+		return "Greetings from Spring Boot! This application has now been dockerized.";
 	}
 
 }
